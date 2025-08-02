2110e4ea27c22d570ce1766e216943666a29ee09
12ball <pub@janie.page>
2025-08-02 09:58:18 -0500

    Refactor

--- calparse.py
+++ calparse.py
@@ -2,46 +2,62 @@ import argparse
 import json
 import os
 import random
+import uuid
 from datetime import datetime, timedelta
 from traceback import print_exception
 
 from icalevents.icalevents import events
 
 
-def parse(mode, urls_path):
-    cal = []
+def display_str(event):
+    return event.start.strftime("%-m/%-d %I:%M %p") + " " + event.summary
+
+
+def pull_and_display(mode, urls_path, cache_path):
+    cal = {}
     with open(urls_path) as f:
         urls = json.load(f)
     start_date = datetime.now()
     end_date = start_date + timedelta(weeks=1)
     for url in urls:
-        cal += events(url, sort=True, start=start_date, end=end_date)
+        for e in events(url, sort=True, start=start_date, end=end_date):
+            cal[e.start.isoformat() + str(uuid.uuid4())] = display_str(e)
+    with open(cache_path, "w") as cache:
+        json.dump(cal, cache)
+    print_result(mode, cal)
+
+
+def print_result(mode, cal):
+    vals = [cal[k] for k in sorted(cal.keys())]
     if mode == "random":
-        e = random.choice(cal)
-        print(e.start.strftime("%-m/%-d %I:%M %p") + " " + e.summary)
+        print(random.choice(vals))
     elif mode == "list":
-        cal = sorted(cal, key=lambda e: e.start)
-        for e in cal:
-            print(e.start.strftime("%-m/%-d %I:%M %p") + " " + e.summary)
+        for e in sorted(cal.values()):
+            print(e)
     elif mode == "sequence":
-        cal = sorted(cal, key=lambda e: e.start)
         minutes = datetime.now().minute
-        e = cal[minutes % len(cal)]
-        print(e.start.strftime("%-m/%-d %I:%M %p") + " " + e.summary)
+        print(vals[minutes % len(vals)])
     else:
         print_exception("bad mode....")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+    argparser = argparse.ArgumentParser(
         prog="calparse", description="Parses iCal URLs in different ways."
     )
-    parser.add_argument("mode", help="supports 'random', 'list'")
-    parser.add_argument(
+    argparser.add_argument("mode", help="supports 'random', 'list'")
+    argparser.add_argument(
         "-u",
         "--urls",
-        help="JSON file containing a list of iCal URLs",
+        help="JSON file containing a list of iCal URLs (default: ./urls.json)",
         default=os.path.join(os.path.dirname(__file__), "urls.json"),
     )
-    args = parser.parse_args()
-    parse(args.mode.lower(), args.urls)
+    homedir = os.environ.get("HOME")
+    argparser.add_argument(
+        "-c",
+        "--cache",
+        help="cache file of calendar state (default $HOME/.cache/calparse-cache.json)",
+        default=os.path.join(homedir, ".cache/calparse-cache.json"),
+    )
+    args = argparser.parse_args()
+    pull_and_display(args.mode.lower(), args.urls, args.cache)
